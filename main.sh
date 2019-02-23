b31d294a00f994e81401fe23d2a8fb8e6c839db7
iosifnicolae2 <iosif@vitaminsoftware.com>
2019-02-23 16:00:59 +0200

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 FLYNN_CMD="/app/flynn"
 CERTBOT_WORK_DIR="/app"
 CERTBOT_CONFIG_DIR="/app/config"
@@ -8,8 +9,8 @@ if [ -z "$CERTBOT_DNS_PLUGIN" ]; then
     exit 1
 fi
 
-if [ -z "$DIGITAL_OCEAN_API_KEY" ]; then
-    echo "DIGITAL_OCEAN_API_KEY must be set"
+if [ -z "$CLOUDFLARE_API_KEY" ]; then
+    echo "$CLOUDFLARE_API_KEY must be set"
     exit 1
 fi
 
@@ -18,8 +19,8 @@ if [ -z "$DOMAINS" ]; then
     exit 1
 fi
 
-if [ -z "$EMAIL" ]; then
-    echo "EMAIL must be set"
+if [ -z "$CLOUDFLARE_EMAIL" ]; then
+    echo "$CLOUDFLARE_EMAIL must be set"
     exit 1
 fi
 
@@ -46,16 +47,17 @@ L="$FLYNN_CMD" && curl -sSL -A "`uname -sp`" https://dl.flynn.io/cli | zcat >$L 
 echo "Adding cluster $FLYNN_CLUSTER_HOST..."
 "$FLYNN_CMD" cluster add -p "$FLYNN_TLS_PIN" default "$FLYNN_CLUSTER_HOST" "$FLYNN_CONTROLLER_KEY"
 
-if [ "$CERTBOT_DNS_PLUGIN" != "digitalocean" ]; then
-   echo "CERTBOT_DNS_PLUGIN only supports 'digitalocean'";
+if [ "$CERTBOT_DNS_PLUGIN" != "cloudflare" ]; then
+   echo "CERTBOT_DNS_PLUGIN only supports 'cloudflare'";
    exit 1;
 fi
 
 # Create file needed for certbot
-echo "Writing Digital Ocean key to disk..."
-DIGITAL_OCEAN_SECRET_PATH="/app/digitalocean.ini"
-echo "dns_digitalocean_token = $DIGITAL_OCEAN_API_KEY" > "$DIGITAL_OCEAN_SECRET_PATH"
-chmod 600 "$DIGITAL_OCEAN_SECRET_PATH"
+echo "Writing Cloudflare key to disk..."
+CLOUDFLARE_SECRET_PATH="/app/cloudflare.ini"
+echo "dns_cloudflare_email = $CLOUDFLARE_EMAIL" >> "$CLOUDFLARE_SECRET_PATH"
+echo "dns_cloudflare_api_key = $CLOUDFLARE_API_KEY" >> "$CLOUDFLARE_SECRET_PATH"
+chmod 600 "$CLOUDFLARE_SECRET_PATH"
 
 # Get domains array
 echo "Collecting domains..."
@@ -80,9 +82,9 @@ echo "certbot certonly \
         --logs-dir $CERTBOT_WORK_DIR/logs \
         --agree-tos \
         --no-eff-email \
-        --dns-digitalocean \
-        --email $EMAIL \
-        --dns-digitalocean-credentials $DIGITAL_OCEAN_SECRET_PATH \
+        --dns-cloudflare \
+        --dns-cloudflare-credentials $CLOUDFLARE_SECRET_PATH \
+        --dns-cloudflare-propagation-seconds 60 \
         -d $COMMA_DOMAINS"
 
 certbot certonly \
@@ -91,9 +93,9 @@ certbot certonly \
   --logs-dir "$CERTBOT_WORK_DIR/logs" \
   --agree-tos \
   --no-eff-email \
-  --dns-digitalocean \
-  --email "$EMAIL" \
-  --dns-digitalocean-credentials "$DIGITAL_OCEAN_SECRET_PATH" \
+  --dns-cloudflare \
+  --dns-cloudflare-credentials $CLOUDFLARE_SECRET_PATH \
+  --dns-cloudflare-propagation-seconds 60 \
   -d "$COMMA_DOMAINS"
 
 echo "Updating Flynn routes..."
@@ -105,10 +107,10 @@ for ROUTE_DOMAIN_PAIR in "${DOMAINS_PAIRS_ARRAY[@]}"
 do
     # Get app name and domain from app:domain pair
     DOMAINS_ARRAY=(${ROUTE_DOMAIN_PAIR//:/ })
-    
+
     APP_NAME="${DOMAINS_ARRAY[0]}"
     DOMAIN="${DOMAINS_ARRAY[1]}"
-    
+
     ROUTE_ID=$("$FLYNN_CMD" -a "$APP_NAME" route | grep "$DOMAIN" | awk '{print $3}')
 
     if [ -z "$ROUTE_ID" ]; then
